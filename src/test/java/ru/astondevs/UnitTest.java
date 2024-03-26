c27a0f926e0e4ad23e46db558817e0cb7a50e8f4
Rafikova Darya <darya.sladkova.89@mail.ru>
2024-03-26 10:49:36 +0400

    add maven

---
+++ src/test/java/ru/astondevs/UnitTest.java
@@ -0,0 +1,12 @@
+package ru.astondevs;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class UnitTest {
+
+    @Test
+    public void positiveNumber() {
+
+    }
+}
