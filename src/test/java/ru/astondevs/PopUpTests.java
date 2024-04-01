f21c1e733ff232739ce9e96cc972bb8e44c9bcb4
Rafikova Darya <darya.sladkova.89@mail.ru>
2024-04-02 00:26:06 +0400

    Lesson_14 1st commit

--- src/test/java/ru/astondevs/PopUpTests.java
+++ src/test/java/ru/astondevs/PopUpTests.java
@@ -1,7 +1,9 @@
 package ru.astondevs;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import ru.astondevs.pageEntity.PageEntity;
@@ -18,8 +20,28 @@ public class PopUpTests extends BaseTest {
     }
 
     @Test
-    public void runPopUp() {
-        PageEntity mts = PageFactory.initElements(driver, PageEntity.class);
-        mts.clickButtonContinue();
+    public void communicationServices() {
+        WebElement selectOption = driver.findElement(By.xpath("//p[contains(text(),'Услуги связи')]"));
+        Assert.assertTrue(selectOption.isEnabled());
+    }
+
+    @Test
+    public void homeInternet() {
+        WebElement selectOption = driver.findElement(By.xpath("//p[contains(text(),'Домашний интернет')]"));
+        Assert.assertTrue(selectOption.isEnabled());
+    }
+
+    @Test
+    public void installments() {
+        WebElement selectOption = driver.findElement(By.xpath("//p[contains(text(),'Рассрочка')]"));
+        Assert.assertTrue(selectOption.isEnabled());
+    }
+
+    @Test
+    public void debt() {
+        WebElement selectOption = driver.findElement(By.xpath("//p[contains(text(),'Задолженность')]"));
+        Assert.assertTrue(selectOption.isEnabled());
     }
 }
+
+
--- src/test/java/ru/astondevs/pageEntity/PageEntity.java
+++ src/test/java/ru/astondevs/pageEntity/PageEntity.java
@@ -6,22 +6,6 @@ import org.openqa.selenium.support.FindBy;
 public class PageEntity {
     @FindBy(xpath = "//button[@id='cookie-agree']")
     private WebElement buttonAgree;
-    @FindBy(xpath = "//h2[contains(text(),'Онлайн пополнение')]")
-    private WebElement blockName;
-    @FindBy(xpath = "//img[@alt='Visa']")
-    private WebElement visaLogo;
-    @FindBy(xpath = "//img[@alt='Verified By Visa']")
-    private WebElement verifiedByVisaLogo;
-    @FindBy(xpath = "//img[@alt='MasterCard']")
-    private WebElement masterCardLogo;
-    @FindBy(xpath = "//img[@alt='MasterCard Secure Code']")
-    private WebElement masterCardSecureLogo;
-    @FindBy(xpath = "//img[@alt='Белкарт']")
-    private WebElement belcardLogo;
-    @FindBy(xpath = "//img[@alt='МИР']")
-    private WebElement mirLogo;
-    @FindBy(xpath = "//a[contains(text(),'Подробнее о сервисе')]")
-    private WebElement aboutService;
     @FindBy(xpath = "//form[@class='pay-form opened']/button[@class='button button__default ']")
     private WebElement buttonContinue;
     @FindBy(xpath = "//input[@class='phone']")
@@ -32,40 +16,6 @@ public class PageEntity {
     public void clickToButtonAgree() {
         if (buttonAgree.isDisplayed()) {
             buttonAgree.click();
-        }
-    }
-
-    public WebElement getBlockName() {
-        return blockName;
-    }
-
-    public boolean visaLogo() {
-        return visaLogo.isDisplayed();
-    }
-
-    public boolean verifiedByVisaLogo() {
-        return verifiedByVisaLogo.isDisplayed();
-    }
-
-    public boolean masterCardLogo() {
-        return masterCardLogo.isDisplayed();
-    }
-
-    public boolean masterCardSecureLogo() {
-        return masterCardSecureLogo.isDisplayed();
-    }
-
-    public boolean belcardLogo() {
-        return belcardLogo.isDisplayed();
-    }
-
-    public boolean mirLogo() {
-        return mirLogo.isDisplayed();
-    }
-
-    public void clickAboutService() {
-        if (aboutService.isDisplayed()) {
-            aboutService.click();
         }
     }
 
@@ -83,3 +33,5 @@ public class PageEntity {
         }
     }
 }
+
+
