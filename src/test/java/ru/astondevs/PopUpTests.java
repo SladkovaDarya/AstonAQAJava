604866ed6d2668cd12f671b9f0e14a1c00b1fc30
Rafikova Darya <darya.sladkova.89@mail.ru>
2024-04-02 20:22:10 +0400

    Lesson_14 1st commit

--- src/test/java/ru/astondevs/PopUpTests.java
+++ src/test/java/ru/astondevs/PopUpTests.java
@@ -3,14 +3,25 @@ package ru.astondevs;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import ru.astondevs.pageEntity.PageEntity;
 
+import java.time.Duration;
+import java.util.List;
+
 public class PopUpTests extends BaseTest {
+    public String spanLocator = "//span[contains(text(),'90.00 BYN')]";
+    public String buttonSumLocator = "//button[@class ='colored disabled ng-star-inserted']";
+    public String phoneTextLocalor = "//p[@class='header__payment-info']";
+    public String slableIconsLocator = "//img[@class='ng-tns-c55-0 ng-star-inserted']";
+    public String switchedIconsLocator = "//img[@class='ng-tns-c55-0 ng-trigger ng-trigger-randomCardState ng-star-inserted']";
+
     @BeforeTest
-    public void sendForm() {
+    public void open() {
         PageEntity mts = PageFactory.initElements(driver, PageEntity.class);
         mts.clickToButtonAgree();
         WebElement phone = mts.getPhone();
@@ -19,28 +30,89 @@ public class PopUpTests extends BaseTest {
         inputPrice.sendKeys("90");
     }
 
-    @Test
+    @Test(priority = 1)
     public void communicationServices() {
-        WebElement selectOption = driver.findElement(By.xpath("//p[contains(text(),'Услуги связи')]"));
+        PageEntity mts = PageFactory.initElements(driver, PageEntity.class);
+        WebElement selectOption = mts.getCommunicationServices();
         Assert.assertTrue(selectOption.isEnabled());
     }
 
-    @Test
+    @Test(priority = 2)
     public void homeInternet() {
-        WebElement selectOption = driver.findElement(By.xpath("//p[contains(text(),'Домашний интернет')]"));
+        PageEntity mts = PageFactory.initElements(driver, PageEntity.class);
+        WebElement selectOption = mts.getHomeInternet();
         Assert.assertTrue(selectOption.isEnabled());
     }
 
-    @Test
+    @Test(priority = 3)
     public void installments() {
-        WebElement selectOption = driver.findElement(By.xpath("//p[contains(text(),'Рассрочка')]"));
+        PageEntity mts = PageFactory.initElements(driver, PageEntity.class);
+        WebElement selectOption = mts.getInstallments();
         Assert.assertTrue(selectOption.isEnabled());
     }
 
-    @Test
+    @Test(priority = 4)
     public void debt() {
-        WebElement selectOption = driver.findElement(By.xpath("//p[contains(text(),'Задолженность')]"));
+        PageEntity mts = PageFactory.initElements(driver, PageEntity.class);
+        WebElement selectOption = mts.getDebt();
         Assert.assertTrue(selectOption.isEnabled());
+    }
+
+    @Test(priority = 5)
+    public void checkSum() {
+        PageEntity mts = PageFactory.initElements(driver, PageEntity.class);
+        mts.clickButtonContinue();
+        WebElement frame = mts.getFrame();
+        driver.switchTo().frame(frame);
+        WebElement span = (new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions
+                        .presenceOfElementLocated(By.xpath(spanLocator))));
+        String expect = "90.00 BYN";
+        Assert.assertEquals(span.getText(), expect);
+    }
+
+    @Test(priority = 6)
+    public void checkSumButton() {
+        WebElement buttonSum = (new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions
+                        .presenceOfElementLocated(By.xpath(buttonSumLocator))));
+        String expect = "Оплатить 90.00 BYN";
+        Assert.assertEquals(buttonSum.getText(), expect);
+    }
+
+    @Test(priority = 7)
+    public void phoneNumber() {
+        WebElement phone = (new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions
+                        .presenceOfElementLocated(By.xpath(phoneTextLocalor))));
+        String expect = "Оплата: Услуги связи Номер:375297777777";
+        Assert.assertEquals(phone.getText(), expect);
+    }
+
+    @Test(priority = 8)
+    public void textForm() {
+        PageEntity mts = PageFactory.initElements(driver, PageEntity.class);
+        List<String> actualText = List.of(mts.getTextCvc().getText(),
+                mts.getTextCardHolder().getText(),
+                mts.getTextNumberCard().getText(),
+                mts.getTextValidityPeriod().getText());
+        List<String> expectedText = List.of("CVC", "Имя держателя (как на карте)",
+                "Номер карты", "Срок действия");
+        Assert.assertEquals(actualText, expectedText);
+    }
+
+    @Test(priority = 9)
+    public void stableIcons() {
+        PageEntity mts = PageFactory.initElements(driver, PageEntity.class);
+        List<WebElement> webElements = driver.findElements(By.xpath(slableIconsLocator));
+        mts.isStableIconsDisplayed(webElements);
+    }
+
+    @Test(priority = 10)
+    public void switchableIcons() {
+        PageEntity mts = PageFactory.initElements(driver, PageEntity.class);
+        List<WebElement> webElements = driver.findElements(By.xpath(switchedIconsLocator));
+        mts.isSwitchedIconsDisplayed(webElements);
     }
 }
 
--- src/test/java/ru/astondevs/pageEntity/PageEntity.java
+++ src/test/java/ru/astondevs/pageEntity/PageEntity.java
@@ -3,6 +3,8 @@ package ru.astondevs.pageEntity;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class PageEntity {
     @FindBy(xpath = "//button[@id='cookie-agree']")
     private WebElement buttonAgree;
@@ -12,6 +14,24 @@ public class PageEntity {
     private WebElement phone;
     @FindBy(xpath = "//input[@class='total_rub']")
     private WebElement inputPrice;
+    @FindBy(xpath = "//p[contains(text(),'Услуги связи')]")
+    private WebElement communicationServices;
+    @FindBy(xpath = "//p[contains(text(),'Домашний интернет')]")
+    private WebElement homeInternet;
+    @FindBy(xpath = "//p[contains(text(),'Рассрочка')]")
+    private WebElement installments;
+    @FindBy(xpath = "//p[contains(text(),'Задолженность')]")
+    private WebElement debt;
+    @FindBy(xpath = "//iframe[@class = 'bepaid-iframe']")
+    private WebElement frame;
+    @FindBy(xpath = "//label[@class='ng-tns-c49-1 ng-star-inserted']")
+    private WebElement textNumberCard;
+    @FindBy(xpath = "//label[@class='ng-tns-c49-4 ng-star-inserted']")
+    private WebElement textValidityPeriod;
+    @FindBy(xpath = "//label[@class='ng-tns-c49-5 ng-star-inserted']")
+    private WebElement textCvc;
+    @FindBy(xpath = "//label[@class='ng-tns-c49-3 ng-star-inserted']")
+    private WebElement textCardHolder;
 
     public void clickToButtonAgree() {
         if (buttonAgree.isDisplayed()) {
@@ -27,10 +47,65 @@ public class PageEntity {
         return inputPrice;
     }
 
+    public WebElement getFrame() {
+        return frame;
+    }
+
+    public WebElement getCommunicationServices() {
+        return communicationServices;
+    }
+
+    public WebElement getHomeInternet() {
+        return homeInternet;
+    }
+
+    public WebElement getInstallments() {
+        return installments;
+    }
+
+    public WebElement getDebt() {
+        return debt;
+    }
+
+    public WebElement getTextNumberCard() {
+        return textNumberCard;
+    }
+
+    public WebElement getTextValidityPeriod() {
+        return textValidityPeriod;
+    }
+
+    public WebElement getTextCvc() {
+        return textCvc;
+    }
+
+    public WebElement getTextCardHolder() {
+        return textCardHolder;
+    }
+
     public void clickButtonContinue() {
         if (buttonContinue.isDisplayed()) {
             buttonContinue.click();
         }
+    }
+
+    public boolean isStableIconsDisplayed(List<WebElement> list) {
+        int cnt = 0;
+        for (WebElement element : list) {
+            if (element.isDisplayed()) {
+                cnt++;
+            }
+        }
+        return cnt == 3;
+    }
+    public boolean isSwitchedIconsDisplayed(List<WebElement> list) {
+        int cnt = 0;
+        for (WebElement element : list) {
+            if (element.isDisplayed()) {
+                cnt++;
+            }
+        }
+        return cnt == 2;
     }
 }
 
